975819d7efd576e007476ae19055c4d262336553
piana <nanusia.d@gmail.com>
2017-09-17 12:15:13 +0200

    modyfikacja szblonu, by pokazywal posty z bazy danych

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+# kropka po from oznaca bieżący katalog/aplikację
+# Jako że pliki views.py i models.py są
+#  w tym katalogu, możemy użyć po prostu . i
+# nazwy pliku (bez .py).
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
